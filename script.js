58ab122e1d5b28b0d80ea0f45bef32d05bb1d6a7
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-06-14 00:04:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = players.slice();
+const cap1 = { ...person };
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+console.log(team);  // Output: ["John", "Bob", "Alice", "Poppy"]
+console.log(team1); // Output: ["John", "Bob", "Alice", "Poppy"]
+console.log(cap1);  // Output: { name: "John Doe", age: 80 }
