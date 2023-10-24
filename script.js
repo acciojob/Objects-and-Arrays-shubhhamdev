9a00037ad4a5c623e0f4f7fc88e4e136f623d649
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-10-24 16:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,22 +4,4 @@ const person = {
   age: 80,
 };
 
-// Create a reference to the players array
-const team = players;
-
-// Create a shallow copy of the players array
-const team1 = [...players];
-
-// Create a shallow copy of the person object
-const cap1 = Object.assign({}, person);
-
-// Testing to ensure the copies are independent
-team.push("New Player");
-team1.push("Another Player");
-cap1.name = "New Name";
-
-console.log("Original Players:", players);
-console.log("Team:", team);
-console.log("Team1:", team1);
-console.log("Original Person:", person);
-console.log("Cap1:", cap1);
+// Write your code here
